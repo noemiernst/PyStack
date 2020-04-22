0fc9a86e5ba039aef4a3ace2aa993f80b455d73c
Noemi Ernst <noemi.ernst@outlook.com>
2020-04-22 20:45:06 +0200

    Change votes and badges table to be saved in DB

--- pre_processing/process_badges.py
+++ pre_processing/process_badges.py
@@ -1,49 +1,50 @@
 try:
-	import xml.etree.cElementTree as ET
+    import xml.etree.cElementTree as ET
 except ImportError:
-	import xml.etree.ElementTree as ET
+    import xml.etree.ElementTree as ET
 import os.path
 import pandas as pd
 import argparse
-from helper_func import sprint
+import sqlite3
 
-def badges_processing(file_name):
-	index = []
-	UserId = [] 
-	BadgeName = []
-	BadgeDate = []
+def badges_processing(file_name, output_file):
+    index = []
+    UserId = []
+    BadgeName = []
+    BadgeDate = []
 
-	for event,elem in ET.iterparse(file_name):
-		if event == "end":
-			try:
-				#print elem.tag,elem.attrib
-				ind = int(elem.attrib["Id"])
-				userid = int(elem.attrib["UserId"])
-				badgename = elem.attrib["Name"]
-				badgedate = elem.attrib["Date"]
-				#print ind,userid,badgename,badgedate
-				index.append(ind)
-				UserId.append(userid)
-				BadgeName.append(badgename)
-				BadgeDate.append(badgedate)
-				elem.clear()
-			except Exception as e:
-				pass
+    for event,elem in ET.iterparse(file_name):
+        if event == "end":
+            try:
+                #print elem.tag,elem.attrib
+                ind = int(elem.attrib["Id"])
+                userid = int(elem.attrib["UserId"])
+                badgename = elem.attrib["Name"]
+                badgedate = elem.attrib["Date"]
+                #print ind,userid,badgename,badgedate
+                index.append(ind)
+                UserId.append(userid)
+                BadgeName.append(badgename)
+                BadgeDate.append(badgedate)
+                elem.clear()
+            except Exception as e:
+                pass
 
-	dir_path = os.path.dirname(os.path.abspath(file_name))
-	output_file = os.path.join(dir_path,"Badges.csv")
-	print("output file: %s" % output_file)
-	df =pd.DataFrame({"UserId":UserId,"BadgeName":BadgeName,"BadgeDate":BadgeDate})
-	df.to_csv(output_file,index = False,columns = ["UserId","BadgeName","BadgeDate"])
-	sprint(dir_path,"pystack_analysis.log","# users having badges: %d" % len(df))
-	
+    df =pd.DataFrame({"UserId":UserId,"BadgeName":BadgeName,"BadgeDate":BadgeDate})
+    DB = sqlite3.connect(output_file)
+    df.to_sql(name='Badges', con=DB, if_exists='replace', index = False)
+    DB.close()
+
 if __name__ == "__main__":
-	'''
-	process */Badges.xml
-	'''
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-i","--input",default= "dataset/ai/Badges.xml", help = "input: */Badges.xml, output: */Badges.csv")
-	args = parser.parse_args()
-	input_file = args.input
-	print("input file %s " % input_file)
-	badges_processing(input_file)
+    '''
+    process */Badges.xml
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--input",default= "dataset/mathematics/Badges.xml", help = "input: */Badges.xml, output: */Badges.csv")
+    parser.add_argument("-d", "--database", default='../database/dataset.db', help="output database")
+    args = parser.parse_args()
+    input_file = args.input
+    database_file = args.database
+    print("input file %s " % input_file)
+    print("database file %s " % database_file)
+    badges_processing(input_file, database_file)
--- pre_processing/process_votes.py
+++ pre_processing/process_votes.py
@@ -1,53 +1,53 @@
 try:
-	import xml.etree.cElementTree as ET
+    import xml.etree.cElementTree as ET
 except ImportError:
-	import xml.etree.ElementTree as ET
+    import xml.etree.ElementTree as ET
 import os.path
 import pandas as pd
 import argparse
-from helper_func import sprint
-def bounty_processing(input_file):
-	d = {"PostId":[],"BountyAmount":[]}
-	for event,elem in ET.iterparse(input_file):
-			if event == "end":
-				try:
-					if "BountyAmount" in elem.attrib:
-						postid = int(elem.attrib["PostId"])
-						bounty = int(elem.attrib["BountyAmount"])
-						d["PostId"].append(postid)
-						d["BountyAmount"].append(bounty)
-					#print elem.tag,elem.attrib
-					elem.clear()
-				except Exception as e:
-					pass
-					#print e
+import sqlite3
 
-	answerid_questionid_file = os.path.join(os.path.dirname(input_file),"AnswerId_QuestionId.csv")
-	answerid_questionid = pd.read_csv(answerid_questionid_file)
+def bounty_processing(input_file, output_file):
+    d = {"PostId":[],"BountyAmount":[]}
+    for event,elem in ET.iterparse(input_file):
+            if event == "end":
+                try:
+                    if "BountyAmount" in elem.attrib:
+                        postid = int(elem.attrib["PostId"])
+                        bounty = int(elem.attrib["BountyAmount"])
+                        d["PostId"].append(postid)
+                        d["BountyAmount"].append(bounty)
+                    #print elem.tag,elem.attrib
+                    elem.clear()
+                except Exception as e:
+                    pass
+                    #print e
 
-	question_bounty = {"QuestionId":[],"Bounty":[]}
-	for postid,bounty in zip(d["PostId"],d["BountyAmount"]):
-		if answerid_questionid[answerid_questionid["QuestionId"] == postid].index.tolist():
-			question_bounty["QuestionId"].append(postid)
-			question_bounty["Bounty"].append(bounty)
-	
-	file_dir = os.path.dirname(os.path.abspath(input_file))
-	output_file = os.path.join(file_dir,"QuestionId_Bounty.csv")
-	df = pd.DataFrame(question_bounty)
-	df.to_csv(output_file,index = False,columns = ["QuestionId","Bounty"])
-	print("***********************************")
-	print("output file: %s" % output_file)
-	sprint(file_dir,"pystack_analysis.log","# questions having bounty: %d" % len(df))
-	print("***********************************")
+    answerid_questionid_file = os.path.join(os.path.dirname(input_file),"AnswerId_QuestionId.csv")
+    answerid_questionid = pd.read_csv(answerid_questionid_file)
+
+    question_bounty = {"QuestionId":[],"Bounty":[]}
+    for postid,bounty in zip(d["PostId"],d["BountyAmount"]):
+        if answerid_questionid[answerid_questionid["QuestionId"] == postid].index.tolist():
+            question_bounty["QuestionId"].append(postid)
+            question_bounty["Bounty"].append(bounty)
+
+    df = pd.DataFrame(question_bounty)
+    DB = sqlite3.connect(output_file)
+    df.to_sql(name='QuestionId_BountyId', con=DB, if_exists='replace', index = False)
+    DB.close()
 
 if __name__ == "__main__":
-	'''
-	process */Votes.xml
-	'''
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-i","--input",default= "../dataset/ai/Votes.xml", help = "input: */Votes.xml, output: */QuestionId_Bounty.csv")
-	args = parser.parse_args()
-	input_file = args.input
-	print("input file %s " % input_file)
-	bounty_processing(input_file)
+    '''
+    process */Votes.xml
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--input",default= "../dataset/mathematics/Votes.xml", help = "input: */Votes.xml, output: */QuestionId_Bounty.csv")
+    parser.add_argument("-d", "--database", default='../database/dataset.db', help="output database")
+    args = parser.parse_args()
+    input_file = args.input
+    database_file = args.database
+    print("input file %s " % input_file)
+    print("database file %s " % database_file)
+    bounty_processing(input_file, database_file)
 
